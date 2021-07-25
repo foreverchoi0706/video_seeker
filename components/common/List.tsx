8c42bb2788c1ba1d4576240442ce2c0e9706b9c5
foreverchoi <foreverchoi0706@gmail.com>
2021-07-26 02:05:01 +0900

    add props

--- components/common/List.tsx
+++ components/common/List.tsx
@@ -1,24 +1,54 @@
-const List = () => {
-    return  <section className="flex flex-col gap-2 p-3">
-    <h3 className="pl-2 border-l-4 border-red-500 ">das</h3>
-    <ul className="flex overflow-x-scroll ">
-      <li> <img className="h-48 w-96 flex-shrink" src="https://cdn.onebauer.media/one/empire-images/articles/5ca1ec3f133d503e3a486a2e/avengers-russian-crop.jpg?format=jpg&amp;quality=80&amp;ratio=16-9&amp;resize=aspectfill" /></li>
-      <li> <img className="h-48 w-96 flex-shrink" src="https://cdn.onebauer.media/one/empire-images/articles/5ca1ec3f133d503e3a486a2e/avengers-russian-crop.jpg?format=jpg&amp;quality=80&amp;ratio=16-9&amp;resize=aspectfill" /></li>
-      <li> <img className="h-48 w-96 flex-shrink" src="https://cdn.onebauer.media/one/empire-images/articles/5ca1ec3f133d503e3a486a2e/avengers-russian-crop.jpg?format=jpg&amp;quality=80&amp;ratio=16-9&amp;resize=aspectfill" /></li>
-      <li> <img className="h-48 w-96 flex-shrink" src="https://cdn.onebauer.media/one/empire-images/articles/5ca1ec3f133d503e3a486a2e/avengers-russian-crop.jpg?format=jpg&amp;quality=80&amp;ratio=16-9&amp;resize=aspectfill" /></li>
-      <li> <img className="h-48 w-96 flex-shrink" src="https://cdn.onebauer.media/one/empire-images/articles/5ca1ec3f133d503e3a486a2e/avengers-russian-crop.jpg?format=jpg&amp;quality=80&amp;ratio=16-9&amp;resize=aspectfill" /></li>
-      <li> <img className="h-48 w-96 flex-shrink" src="https://cdn.onebauer.media/one/empire-images/articles/5ca1ec3f133d503e3a486a2e/avengers-russian-crop.jpg?format=jpg&amp;quality=80&amp;ratio=16-9&amp;resize=aspectfill" /></li>
-      <li> <img className="h-48 w-96 flex-shrink" src="https://cdn.onebauer.media/one/empire-images/articles/5ca1ec3f133d503e3a486a2e/avengers-russian-crop.jpg?format=jpg&amp;quality=80&amp;ratio=16-9&amp;resize=aspectfill" /></li>
-      <li> <img className="h-48 w-96 flex-shrink" src="https://cdn.onebauer.media/one/empire-images/articles/5ca1ec3f133d503e3a486a2e/avengers-russian-crop.jpg?format=jpg&amp;quality=80&amp;ratio=16-9&amp;resize=aspectfill" /></li>
-      <li> <img className="h-48 w-96 flex-shrink" src="https://cdn.onebauer.media/one/empire-images/articles/5ca1ec3f133d503e3a486a2e/avengers-russian-crop.jpg?format=jpg&amp;quality=80&amp;ratio=16-9&amp;resize=aspectfill" /></li>
-      <li> <img className="h-48 w-96 flex-shrink" src="https://cdn.onebauer.media/one/empire-images/articles/5ca1ec3f133d503e3a486a2e/avengers-russian-crop.jpg?format=jpg&amp;quality=80&amp;ratio=16-9&amp;resize=aspectfill" /></li>
-      <li> <img className="h-48 w-96 flex-shrink" src="https://cdn.onebauer.media/one/empire-images/articles/5ca1ec3f133d503e3a486a2e/avengers-russian-crop.jpg?format=jpg&amp;quality=80&amp;ratio=16-9&amp;resize=aspectfill" /></li>
-      <li> <img className="h-48 w-96 flex-shrink" src="https://cdn.onebauer.media/one/empire-images/articles/5ca1ec3f133d503e3a486a2e/avengers-russian-crop.jpg?format=jpg&amp;quality=80&amp;ratio=16-9&amp;resize=aspectfill" /></li>
-      <li> <img className="h-48 w-96 flex-shrink" src="https://cdn.onebauer.media/one/empire-images/articles/5ca1ec3f133d503e3a486a2e/avengers-russian-crop.jpg?format=jpg&amp;quality=80&amp;ratio=16-9&amp;resize=aspectfill" /></li>
-      <li> <img className="h-48 w-96 flex-shrink" src="https://cdn.onebauer.media/one/empire-images/articles/5ca1ec3f133d503e3a486a2e/avengers-russian-crop.jpg?format=jpg&amp;quality=80&amp;ratio=16-9&amp;resize=aspectfill" /></li>
-      <li> <img className="h-48 w-96 flex-shrink" src="https://cdn.onebauer.media/one/empire-images/articles/5ca1ec3f133d503e3a486a2e/avengers-russian-crop.jpg?format=jpg&amp;quality=80&amp;ratio=16-9&amp;resize=aspectfill" /></li>
-    </ul>
-  </section>   
+import React from "react";
+import { GetServerSideProps } from "next";
+import { useEffect } from "react";
+import axios from "axios";
+
+interface Props {
+  theme: string;
 }
+
+const List = ({ theme }: Props) => {
+  const search = async (theme: string) => {
+    const { data } = await axios.get("http://localhost:3001/vseeker");
+    console.log(data);
+    
+  };
+
+  useEffect(() => {
+    search(theme);
+  }, []);
+
+  return (
+    <section className="flex flex-col gap-2 p-3">
+      <h3 className="pl-2 border-l-4 border-red-500 ">das</h3>
+      <ul className="flex overflow-x-scroll">
+        <li className="p-1">
+          <img
+            className="h-40 w-32 flex-shrink rounded-md"
+            src="https://cdn.onebauer.media/one/empire-images/articles/5ca1ec3f133d503e3a486a2e/avengers-russian-crop.jpg?format=jpg&amp;quality=80&amp;ratio=16-9&amp;resize=aspectfill"
+          />
+        </li>
+        <li className="p-1">
+          <img
+            className="h-40 w-32 flex-shrink rounded-md"
+            src="https://cdn.onebauer.media/one/empire-images/articles/5ca1ec3f133d503e3a486a2e/avengers-russian-crop.jpg?format=jpg&amp;quality=80&amp;ratio=16-9&amp;resize=aspectfill"
+          />
+        </li>
+        <li className="p-1">
+          <img
+            className="h-40 w-32 flex-shrink rounded-md"
+            src="https://cdn.onebauer.media/one/empire-images/articles/5ca1ec3f133d503e3a486a2e/avengers-russian-crop.jpg?format=jpg&amp;quality=80&amp;ratio=16-9&amp;resize=aspectfill"
+          />
+        </li>
+        <li className="p-1">
+          <img
+            className="h-40 w-32 flex-shrink rounded-md"
+            src="https://cdn.onebauer.media/one/empire-images/articles/5ca1ec3f133d503e3a486a2e/avengers-russian-crop.jpg?format=jpg&amp;quality=80&amp;ratio=16-9&amp;resize=aspectfill"
+          />
+        </li>
+      </ul>
+    </section>
+  );
+};
 
 export default List;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { ChangeEvent } from "react";
 import axios from "axios";
 import {
   GetServerSideProps,
@@ -5,6 +6,7 @@ import {
   GetStaticPropsContext,
 } from "next";
 import Head from "next/head";
+import { useState } from "react";
 import { useCallback } from "react";
 //components
 import Layout from "../components/common/Layout";
@@ -14,8 +16,16 @@ interface Props {
   data: string;
 }
 
+const Modal = () => {};
+
 const Home = ({ data }: Props) => {
-  const clickBtn = useCallback((e) => {}, []);
+  const [keyword, setKeyword] = useState<string>("");
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setKeyword(() => e.target.value);
+  };
+
+  const handleClick = useCallback((e) => {}, []);
 
   return (
     <Layout>
@@ -25,21 +35,29 @@ const Home = ({ data }: Props) => {
         </Head>
         <section className="relative">
           <img
-            onClick={clickBtn}
+            onClick={handleClick}
             src="https://cdn.onebauer.media/one/empire-images/articles/5ca1ec3f133d503e3a486a2e/avengers-russian-crop.jpg?format=jpg&amp;quality=80&amp;ratio=16-9&amp;resize=aspectfill"
           />
-          <div className="absolute bottom-5 w-full flex justify-center">
+          <div className="absolute bottom-5 w-full flex flex-col items-center">
             <input
               type="text"
               className="focus:outline-none w-4/6 rounded-md p-1"
+              value={keyword}
+              onChange={handleChange}
             />
+            {keyword && (
+              <div className="bg-red-500 absolute -bottom-5 w-4/6">
+                dasdsad dasdsad Lorem, ipsum dolor sit amet consectetur
+                adipisicing elit. Placeat deleniti sunt dignissimos commodi
+                ducimus molestias ab porro animi similique eaque corporis
+                inventore, at quidem deserunt natus consequatur omnis? Fuga,
+                voluptatibus?
+              </div>
+            )}
           </div>
         </section>
 
-        <List />
-        <List />
-        <List />
-        <List />
+        <List theme={"test"} />
       </article>
     </Layout>
   );
@@ -49,6 +67,9 @@ export const getServerSideProps = async (context: GetServerSideProps) => {
   const { data } = await axios.get(
     "https://cdn.onebauer.media/one/empire-images/articles/5ca1ec3f133d503e3a486a2e/avengers-russian-crop.jpg?format=jpg&quality=80&width=960&height=540&ratio=16-9&resize=aspectfill"
   );
+
+  
+
   return {
     props: {
       data,
